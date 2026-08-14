8676de6237ba9f1add112eea48c6d721ebe1e628
Dhruv-byte-png <nvidiadhruv11@gmail.com>
2026-08-14 14:25:53 +0530

    Time: 8 ms (57.61%) | Memory: 26.3 MB (37.23%) - LeetSync

---
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        int n = words.size();
+        vector<int> mask(n,0);
+        vector<int> len(n);
+
+        for(int i=0 ; i<n ; i++){
+            int m=0;
+            for(char c  : words[i])
+                m |= (1<<(c-'a'));
+
+            mask[i] = m;
+            len[i] = words[i].size();
+        }
+
+        int ans=0;
+        for(int i=0 ; i<n ; i++){
+            for(int j=i ; j<n ; j++){
+                if((mask[i]&mask[j])==0)
+                    ans = max(ans,len[i]*len[j]);
+            }
+        }
+        return ans;
+    }
+};
