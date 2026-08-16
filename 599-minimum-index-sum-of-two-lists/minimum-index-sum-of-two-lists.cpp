b89683da6212162e55c9f79e6399af9d78e97ba4
Dhruv-byte-png <nvidiadhruv11@gmail.com>
2026-08-16 17:56:31 +0530

    Time: 11 ms (91.89%) | Memory: 41.2 MB (63.75%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
@@ -2,40 +2,24 @@ class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
         
-        map<string , int> str;
-        str[list1[0]] = INT_MIN;
-        for(int i= 1 ; i< list1.size(); i++)
-            str[list1[i]] = -i; 
+        unordered_map<string , int> map;
+        for(int i=0 ; i < list1.size(); i++)
+            map[list1[i]] = i;
 
-        if(!str.contains(list2[0]))
-            str[list2[0]]= INT_MIN;
-
-        else{
-            if(str[list2[0]] != INT_MIN)
-                str[list2[0]] = abs(str[list2[0]]);
-            else
-                str[list2[0]] = 0;
-        }
-
-        for(int i=1 ; i < list2.size(); i++){
-            if(str.contains(list2[i])){
-                if(str[list2[i]] != INT_MIN)
-                    str[list2[i]] = abs(str[list2[i]]) + i;
-                else str[list2[i]] = i;
-            }
-            else str[list2[i]] = -i;
-        }
-
-        int min_Val = INT_MAX;
         vector<string> ans;
+        int sum = INT_MAX;
 
-        for(auto& i : str){
-            if(i.second >=0 && i.second < min_Val)
-                min_Val = i.second;
-        }
-        for(auto& i : str){
-            if(i.second >=0 && i.second <= min_Val)
-                ans.push_back(i.first);
+        for(int i=0 ; i< list2.size() ; i++){
+            if(map.count(list2[i])== 1){
+                int currSum = map[list2[i]] + i;
+                if(currSum < sum){
+                    sum = currSum;
+                    ans.clear();
+                    ans.push_back(list2[i]);
+                }
+                else if(currSum == sum)
+                    ans.push_back(list2[i]);
+            }
         }
         return ans;
     }
