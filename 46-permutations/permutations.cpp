480915a06ea79a99bd0f0e5839a0d3c4f6e243b4
Dhruv-byte-png <nvidiadhruv11@gmail.com>
2026-08-19 23:45:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (89.13%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,26 +1,22 @@
 class Solution {
+
+public:
+    void backtrack(vector<int> & nums , vector<vector<int>>& res , int first ){
+        if(first == nums.size()){
+            res.push_back(nums);
+            return ;
+        }
+        for(int i=first ; i<nums.size(); ++i){
+            swap(nums[first] , nums[i]);
+            backtrack(nums , res , first+1);
+            swap(nums[first] , nums[i]);
+        }
+    }
+
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        if(nums.size() == 1){
-            vector<int> l1;
-            l1.push_back(nums[0]);
-            res.push_back(l1);
-            return res;
-        }
-        for(int i=0 ; i<nums.size() ; i++){
-            int n = nums[i];
-            vector<int> rem_nums;
-            for(int j=0; j<nums.size(); j++){
-                if(j!=i) rem_nums.push_back(nums[j]);
-            }
-
-            vector<vector<int>> permu = permute(rem_nums);
-            for(vector<int> i : permu){
-                i.insert(i.begin(), n);
-                res.push_back(i);
-            }
-        }
+        backtrack(nums,res,0);
         return res;
     }
 };
