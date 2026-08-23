2e332ff8e8d8fab223e92d82fb0d80990641006e
Dhruv-byte-png <nvidiadhruv11@gmail.com>
2026-08-23 20:45:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (25.70%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> cnt(26,0);
+        for(int i=0 ; i<s.length() ; i++) cnt[s[i]-'a'] ++;
+
+        for(int i=0 ; i<t.length() ; i++) {
+            cnt[t[i]-'a'] --;
+            if(cnt[t[i]-'a']==-1){
+                return t[i];
+            }
+        }
+        return 0;
+    }
+};
